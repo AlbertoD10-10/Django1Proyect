8a4cd7c524a4a9eb8ea04f3a357479c6e05020e3
AlbertoD10-10 <albertoandres.diazmejia@gmail.com>
2023-08-28 18:55:31 -0500

    actualizacion taller 2

--- movie/views.py
+++ movie/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
 
 # Create your views here.
 
 def home(request):
     #return HttpResponse('<h1>Welcome to home Page</h1>')
-    return render(request,'home.html',{'name':'Alberto Diaz'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title_icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request,'home.html',{'searchTerm':searchTerm,'movies':movies})
+
 
 def About(request):
-    #return HttpResponse('<h1>Welcome to home Page</h1>')
-    return render(request,'About.html',{'name':'Alberto Andres Diaz Mejia'})     
+    return render(request,'About.html')     
